04f4ca00d3b1a3f540a68f5a09569bc5e37ff790
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-04-01 21:35:42 -0700

    first_commit

---
+++ Bomb.java
@@ -0,0 +1,17 @@
+
+public class Bomb { //ボンバーマンのクラス
+    //フォールドの設定
+    String name;
+    String sex;
+    int old;
+
+    //コンストラクタ　初期化を行った
+    public Bomb(String name, String sex, int old){
+        this.name = name;
+        this.sex = sex;
+        this.old = old;
+    };
+
+    //メソッドの作成
+    
+}
